6934c31d37b6bf827fcf02c357ddbd9fa0199a5a
Wyattwicks <wyattwicks@gmail.com>
2021-07-11 13:03:40 -0600

    Create databases and relationships

---
+++ spec/models/locations_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+describe Location, type: :model do
+  describe 'relationships' do
+    it {should have_many(:users)}
+  end
+end
---
+++ spec/models/users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+describe User, type: :model do
+  # describe "Validations" do
+  #   it {should validate_presence_of :email}
+  #   it {should validate_presence_of :password}
+  #   it {should validate_uniqueness_of(:email).on(:create)}
+  # end
+  describe 'relationships' do
+    it {should belong_to(:location)}
+  end
+
+  # describe "email format" do
+  #   it "create a user with a valid email format" do
+  #     user = User.create!(email: 'harrison@email.com', password: 'password')
+  #     expect(user.email).to eq('harrison@email.com')
+  #   end
+  #   it "should not create a user with a invalid email format" do
+  #     user = User.new(email: 'harrison', password: 'password')
+  #     expect(user.save).to eq(false)
+  #   end
+  # end
+end
