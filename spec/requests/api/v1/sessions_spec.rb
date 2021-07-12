fa2425d117b8633af036f3f5c12f00a558deaffc
Harrison Blake <harrison.r.blake.gmail.com>
2021-07-12 12:05:57 -0600

    Add email and password_digest column to user and start sessions controller

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
-describe 'Users API' do
+describe 'Sessions API' do
   describe 'Happy Path' do
-    it "returns a 201 status and correct structure response" do
-      user_params = ({
-                  email: 'testemail@test.com',
-                  password: 'test',
-                  password_confirmation: 'test'
-                })
+    it "returns correct user data in the correct structure" do
+      user_params = {
+                      email: 'testemail@test.com',
+                      password: 'test',
+                    }
 
       headers = {"CONTENT_TYPE" => "application/json"}
 
+      defualt_location = Location.create!(city: "n/a", state: "n/a")
+
+      User.create!(
+                    email: 'testemail111@test.com',
+                    password: 'test',
+                    location: defualt_location
+                  )
+
       VCR.use_cassette('user_login') do
-        post '/api/v1/users', headers: headers, params: JSON.generate(user: user_params)
+        post '/api/v1/sessions', headers: headers, params: user_params.to_json
       end
+
+      user = JSON.parse(response.body, symbolize_names: true)
+
+      expect(user[:data]).to have_key(:id)
+      expect(user[:data]).to have_key(:type)
+      expect(user[:data]).to have_key(:attributes)
+      expect(user[:data][:attributes]).to have_key(:email)
+      expect(user[:data][:attributes]).to have_key(:api_key)
     end
   end
 end
